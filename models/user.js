c98debe982b02d9205ebe5ded3b7820cfb703b68
Jonathan <jalcantara1990@gmail.com>
2017-06-21 10:30:58 +0200

    Model User defined

---
+++ models/user.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = Schema({
+    name:String,
+    surname: String,
+    nickname:String,
+    bio: String,
+    email: String,
+    password: String,
+    role: String,
+    avatarImage: String,
+    backgroundImage: String,
+    friends: [{ type: Schema.ObjectId, ref: 'Users' }]
+});
+
+module.exports = mongoose.model('User', UserSchema);
